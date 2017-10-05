7c49f18efe6950a565d9f989b15b513e0d5a717d
SunnyNagam <sunnynagam1@gmail.com>
2017-10-05 11:57:34 -0600

    fixed bug to correctly refrence image resources

--- src/Pictures.java
+++ src/Pictures.java
@@ -22,15 +22,15 @@ public class Pictures {
 		plat = mc.getImage(url,"pictures/Platform.png");
 		Jell = mc.getImage(url,"pictures/JellyOver.png");
 		cloud = mc.getImage(url,"pictures/CandyCloud.png");
-		llama[0][0] = mc.getImage(url,"pictures/Llama[0].png");
-		llama[0][1] = mc.getImage(url,"pictures/Llama[1].png");
-		llama[0][2] = mc.getImage(url,"pictures/Llama[2].png");
-		llama[1][0] = mc.getImage(url,"pictures/LamCh[0].png");
-		llama[1][1] = mc.getImage(url,"pictures/LamCh[1].png");
-		llama[1][2] = mc.getImage(url,"pictures/LamCh[2].png");
-		llama[2][0] = mc.getImage(url,"pictures/Tig[0].png");
-		llama[2][1] = mc.getImage(url,"pictures/Tig[1].png");
-		llama[2][2] = mc.getImage(url,"pictures/Tig[2].png");
+		llama[0][0] = mc.getImage(url,"pictures/Llama_0_.png");
+		llama[0][1] = mc.getImage(url,"pictures/Llama_1_.png");
+		llama[0][2] = mc.getImage(url,"pictures/Llama_2_.png");
+		llama[1][0] = mc.getImage(url,"pictures/LamCh_0_.png");
+		llama[1][1] = mc.getImage(url,"pictures/LamCh_1_.png");
+		llama[1][2] = mc.getImage(url,"pictures/LamCh_2_.png");
+		llama[2][0] = mc.getImage(url,"pictures/Tig_0_.png");
+		llama[2][1] = mc.getImage(url,"pictures/Tig_1_.png");
+		llama[2][2] = mc.getImage(url,"pictures/Tig_2_.png");
 		ground = mc.getImage(url,"pictures/Ground.png");
 
 		
